01696c4c0687928706ce2dc027944ccf35dbd8e4
subroh0508 <subroh.0508@gmail.com>
2023-08-27 12:58:44 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("multiplatform") version "1.8.20"
+}
+
+group = "net.subroh0508"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    wasm {
+        binaries.executable()
+        browser {
+
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val wasmMain by getting
+        val wasmTest by getting
+    }
+}
