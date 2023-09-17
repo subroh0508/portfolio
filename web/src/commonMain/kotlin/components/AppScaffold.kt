905405c17bcdc4f09b241b9f692e3cfacddec256
subroh0508 <subroh.0508@gmail.com>
2023-09-18 02:13:07 +0900

    Add Card to Bio

--- web/src/commonMain/kotlin/components/AppScaffold.kt
+++ web/src/commonMain/kotlin/components/AppScaffold.kt
@@ -1,11 +1,14 @@
 package components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -13,8 +16,22 @@ fun AppScaffold(
     content: @Composable () -> Unit,
 ) = Scaffold(
     topBar = { TopAppBar() },
-) {
-    Column(
-        modifier = Modifier.padding(it),
-    ) { content() }
+) { paddingValues ->
+    BoxWithConstraints {
+        val width = when {
+            maxWidth < 640.dp -> maxWidth
+            else -> 640.dp
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .padding(paddingValues),
+        ) {
+            Column(
+                modifier = Modifier.width(width)
+                    .fillMaxHeight()
+                    .align(Alignment.Center),
+            ) { content() }
+        }
+    }
 }
--- web/src/commonMain/kotlin/page/Biography.kt
+++ web/src/commonMain/kotlin/page/Biography.kt
@@ -1,16 +1,38 @@
 package page
 
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import components.AsyncImage
 
 @Composable
 fun Biography() {
-    AsyncImage(
-        "icon.webp",
-        "Subroh Nishikori's Icon",
-        modifier = Modifier.size(48.dp)
-    )
+    Card(
+        modifier = Modifier.fillMaxWidth()
+            .padding(16.dp),
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+        ) {
+            AsyncImage(
+                "icon.webp",
+                "Subroh Nishikori's Icon",
+                modifier = Modifier.size(96.dp)
+                    .clip(CircleShape),
+            )
+
+            Spacer(Modifier.size(16.dp))
+
+            Text(
+                "Subroh Nishikori",
+                style = MaterialTheme.typography.headlineMedium,
+            )
+        }
+    }
 }
