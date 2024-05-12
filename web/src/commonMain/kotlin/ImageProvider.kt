cead588f52af9ecc153cde8b23be99a2c9ae2be4
subroh0508 <subroh.0508@gmail.com>
2024-05-13 00:10:21 +0900

    Change theme color and delete unnecessary files

--- web/src/commonMain/kotlin/ImageProvider.kt
+++
@@ -1,7 +0,0 @@
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import portfolio.web.generated.resources.Res
-
-object ImageProvider {
-    @OptIn(ExperimentalResourceApi::class)
-    suspend fun getImageBitmap(filename: String) = Res.readBytes("drawable/$filename").toImageBitmap()
-}
