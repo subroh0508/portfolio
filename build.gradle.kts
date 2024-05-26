fa8bcb8ad52244d14b55734825650e0acad7aa84
subroh0508 <subroh.0508@gmail.com>
2024-05-26 20:07:09 +0900

    Enable detekt.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
+plugins {
+    alias(libs.plugins.kotlin.mpp) apply false
+    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.buildconfig) apply false
+
+    alias(libs.plugins.detekt)
 }
 
 val wasmJsBrowserTestReport by tasks.registering(TestReport::class) {
@@ -16,8 +17,35 @@ val wasmJsBrowserTestReport by tasks.registering(TestReport::class) {
     }
 }
 
-plugins {
-    alias(libs.plugins.kotlin.mpp) apply false
-    alias(libs.plugins.compose) apply false
-    alias(libs.plugins.buildconfig) apply false
+dependencies {
+    detektPlugins(libs.detekt.formatting)
+}
+
+val detektAll by tasks.registering(Detekt::class) {
+    description = "Runs the Lint check whole project at once."
+
+    basePath = rootProject.projectDir.absolutePath
+    parallel = true
+    autoCorrect = true
+    setSource(files(rootProject.projectDir))
+    config.setFrom(listOf(rootProject.files("config/detekt.yml")))
+
+    include("**/*.kt", "**/**.kts")
+    exclude("**/resources/**", "**/composeResources/**","**/build/**")
+
+    reports {
+        html {
+            required.set(true)
+            outputLocation.set(rootProject.file("lint-reports/kotlin/detekt.html"))
+        }
+
+        sarif {
+            required.set(true)
+            outputLocation.set(rootProject.file("lint-reports/kotlin/detekt.sarif"))
+        }
+
+        txt {
+            required.set(false)
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,12 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
 include(":web")
